28168e12ce1eea15b0e85dea6028047eaafae29b
Oscar <oscar.munoz103@alu.ulpgc.es>
2023-12-15 15:20:42 +0000

    LetterCounterCommand class finished

--- src/main/java/software/ulpgc/kata5/LetterCounterCommand.java
+++ src/main/java/software/ulpgc/kata5/LetterCounterCommand.java
@@ -3,6 +3,24 @@ package software.ulpgc.kata5;
 public class LetterCounterCommand implements Command{
     @Override
     public Output execute(Input input) {
-        return null;
+        return execute(input.get(":parameter"));
+    }
+
+    private Output execute(String word) {
+        return new Output() {
+            @Override
+            public String response() {
+                return Integer.toString(calculate(word));
+            }
+
+            @Override
+            public int responseCode() {
+                return 200;
+            }
+        };
+    }
+
+    private int calculate(String word) {
+        return word.length();
     }
 }
